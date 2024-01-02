341579280e427a9b9bab0461a79b88b8a2ba3a15
Thiago Martins Dantas Silva <thiagomarins.dantas@gmail.com>
2024-01-02 05:55:23 -0300

    :wrench: Add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/ThiagoMDantas/gopportunities/router"
+import (
+	"github.com/ThiagoMDantas/gopportunities/config"
+	"github.com/ThiagoMDantas/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err:= config.Init()
+	if err != nil {
+		logger.Errorf("Config initilization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
