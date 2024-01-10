a67848211ccc9e21a1e144f1829136bb950c07b1
Thiago Martins Dantas Silva <thiagomarins.dantas@gmail.com>
2024-01-10 01:29:07 -0300

    :sparkles: Add logic to update a record

--- handler/updateOpeningHandler.go
+++ handler/updateOpeningHandler.go
@@ -1,13 +1,60 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/ThiagoMDantas/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening...",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequeed("id", "queryParameter").Error())
+		return
+	}
+
+	request := UpdateOpening{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", id)
+		sendError(ctx, http.StatusInternalServerError, "erro updating opening")
+		return
+	}
+
+	sendSuccess(ctx, "update-opening", opening)
+
 }
