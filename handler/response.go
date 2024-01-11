dbc48b8a1eb4f64f0005e9a794a01ecc3e0eb560
Thiago Martins Dantas Silva <thiagomarins.dantas@gmail.com>
2024-01-10 23:49:43 -0300

    :books: Finish swagger data response

--- handler/response.go
+++ handler/response.go
@@ -30,26 +30,26 @@ type ErrorResponse struct {
 }
 
 type CreateOpeningResponse struct {
-	Message string `json: "message"`
-	Data    schemas.OpeningResponse
+	Message string                  `json: "message"`
+	Data    schemas.OpeningResponse `json: "data"`
 }
 
 type DeleteOpeningResponse struct {
-	Message string `json: "message"`
-	Data    schemas.OpeningResponse
+	Message string                  `json: "message"`
+	Data    schemas.OpeningResponse `json: "data"`
 }
 
 type ShowOpeningResponse struct {
-	Message string `json: "message"`
-	Data    schemas.OpeningResponse
+	Message string                  `json: "message"`
+	Data    schemas.OpeningResponse `json: "data"`
 }
 
 type ListOpeningResponse struct {
-	Message string `json: "message"`
-	Data    []schemas.OpeningResponse
+	Message string                    `json: "message"`
+	Data    []schemas.OpeningResponse `json: "data"`
 }
 
 type UpdateOpeningResponse struct {
-	Message string `json: "message"`
-	Data    schemas.OpeningResponse
+	Message string                  `json: "message"`
+	Data    schemas.OpeningResponse `json: "data"`
 }
